2f7f352e90333eaa8cd952a4aa20b7990430106b
Deepankar Singh <deepankarsingh1@gmail.com>
2025-03-31 23:32:32 +0530

    new update

--- lecture - 4/patterns.cpp
+++ lecture - 4/patterns.cpp
@@ -32,16 +32,26 @@ int main() {
     // }
     // return 0;
 
-
-    int n = 3;
+            // reverse number pattern
+    /*int n = 7;
     for (int i = 0; i < n; i++){
-        for (int j = 0; j < i+1; j++){
-            cout << i+1 ;
+        for (int j = i+1; j > 0; j--){
+            cout << j << " ";
         }
         cout << endl;
     }
+    return 0;*/
+
+
+    int n = 5;
+    for (int i = 0; i < n; i++){
+        for(int j = 0; j<i; j++){
+            cout << " ";
+        }for (int j = 0; j < n-i; j++){
+            cout << i+1;
+        }
+       cout << endl;
+       }
     return 0;
-
-
-    
+           
 }
